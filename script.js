4c133ba6d3e15b7fdf689adb4385ea783d691532
NishadHossainEmon <nishad.hossain.emon.77@gmail.com>
2026-02-22 23:25:17 +0600

    added interview and rejected button features

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const buttons = document.querySelectorAll(".filter-btn");
 buttons.forEach(function (btn) {
-  btn.addEventListener("click", function () {
+  btn.addEventListener("click", function () 
+  {
     buttons.forEach(function (b) {
       b.classList.remove("bg-blue-500", "text-white");
       b.classList.add("text-gray-600", "border", "border-gray-300");
@@ -8,19 +9,23 @@ buttons.forEach(function (btn) {
     btn.classList.add("bg-blue-500", "text-white");
     btn.classList.remove("text-gray-600", "border", "border-gray-300");
 
-    let jobCards = document.getElementById('job-cards')
-    let emptyCard = document.getElementById('empty-card')
-    if(btn.innerText == "Interview" || btn.innerText == "Rejected")
-    {
-        jobCards.classList.add('hidden')
-        emptyCard.classList.remove('hidden')
-    }
-    else
-    {   
-        jobCards.classList.remove('hidden')
-        emptyCard.classList.add('hidden')
+    let jobCards = document.getElementById('job-cards');
+    let emptyCard = document.getElementById('empty-card');
+
+    if (btn.innerText == "Interview" || btn.innerText == "Rejected")
+       {
+      jobCards.classList.add('hidden');
+      emptyCard.classList.remove('hidden');
+    } else 
+      {
+      jobCards.classList.remove('hidden');
+      emptyCard.classList.add('hidden');
     }
   });
 });
 
-
+function showBadge(showId, hideId1, hideId2) {
+  document.getElementById(showId).classList.remove('hidden'); 
+  document.getElementById(hideId1).classList.add('hidden');    
+  document.getElementById(hideId2).classList.add('hidden');    
+}
